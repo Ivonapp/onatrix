0a7e68036e4cddda9f377109bcf82a47e898c6bc
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-11-03 16:04:32 +0100

    buttonlight

---
+++ src/components/Buttonlight.jsx
@@ -0,0 +1,12 @@
+      import React from 'react';
+      import "./Adesign.css";
+
+    export default function Buttonlight({ text }) {
+        return (
+        
+      <div>
+      <button className="buttonlight">{text}</button>
+      </div>
+
+      )
+      }
