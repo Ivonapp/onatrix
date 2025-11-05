4c174f284c2fe541fa9e547aa64b90476acbd4e8
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-11-05 16:49:14 +0100

    blogaccordion updated

--- src/components/Blogaccordion.jsx
+++ src/components/Blogaccordion.jsx
@@ -14,12 +14,11 @@ const toggleblog = () => {
     
     <div className ="blog-accordion-item">
     <div className={`item-blog-title ${isOpen ? 'active': ''}`} onClick={toggleblog}>
-    {isOpen ? item.description : item.description.substring(0, 100) + '...'}
+    <div className="column-ingress">{isOpen ? item.description : item.description.substring(0, 100) + '...'}</div> {/*{isOpen ? item.description : item.description.substring(0, 100) + '...'} <- Denna kodraden för att dölja lite av texten i blogcolumns är dels med CHATGPT*/}
     <div className="column-yellow-ingress"> {isOpen ? 'Read less' : 'read more →'}</div>
   
 
-    </div>
-
+        </div>
     </div>
   )
 }
