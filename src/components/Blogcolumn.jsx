e864d409a935c59a018454f93056e3750ba3b041
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-11-05 01:57:57 +0100

    Blogcolumn update

--- src/components/Blogcolumn.jsx
+++ src/components/Blogcolumn.jsx
@@ -1,5 +1,6 @@
 
 import React, {useState, useEffect} from 'react'
+import Blogaccordion from './Blogaccordion'
 
 const Blogcolumn = () => {
     const [serviceItems, setServiceItems] = useState([])
@@ -17,20 +18,20 @@ const Blogcolumn = () => {
         <div className="container">  
 <div className="column-blog-container">
 
-
-           {serviceItems.map(item => (
-            <div key={item.id} className="column-wrapper">
+    {serviceItems.map(item => (
+    <div key={item.id} className="column-wrapper">
     <div className="column-green-1">
-    <div className="column-img">
+<div className="column-img">
         <img src={item.imageUrl} />
     </div>
-  
-    <div className="column-icon-datum"> 
-    <i className="bi bi-calendar-range"></i>
-                              <span className="kalender-text">{item.created}</span></div>
-                            <h6 className="column-title">{item.title}</h6>
-                      <div className="column-ingress">{item.description}</div>
-                    <div className="column-yellow-ingress">Read more →</div>
+
+      <div className="column-icon-datum"> 
+        <i className="bi bi-calendar-range"></i>
+          <span className="kalender-text">{item.created}</span></div>
+              <h6 className="column-title">{item.title}</h6>
+
+              <Blogaccordion item={item} />
+              
                 </div>
             </div>
           ))}
