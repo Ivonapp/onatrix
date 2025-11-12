90581022a33217d27307879a36be3ef28166b2c0
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-11-12 20:04:58 +0100

    WCAG update, added description to image

--- src/components/Chooseus.jsx
+++ src/components/Chooseus.jsx
@@ -4,7 +4,7 @@ import Icon2 from '../assets/images/Icon2.svg';
 import Icon3 from '../assets/images/Icon3.svg';
 import Icon4 from '../assets/images/Icon4.svg';
 import ChooseusColumn from './ChooseusColumn';
-import Chooseusbox from '../assets/images/Chooseusbox.svg';
+import Chooseusbox from '../assets/images/Chooseusbox.jpg';
 import Titlelayout from './Titlelayout';
 
 function Chooseus() {
