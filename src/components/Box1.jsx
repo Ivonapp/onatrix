8a1bd9998c0542a07925046820406c4cff597fd4
Ivonapp <ivona.v.hellsten@hotmail.com>
2025-11-13 15:33:35 +0100

    WCAG update for box1

--- src/components/Box1.jsx
+++ src/components/Box1.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Box2 from '../components/Box2'
 import Positionering1 from '../assets/images/Positionering1.jpg';
 import Positionering2 from '../assets/images/Positionering2.svg';
-
 function Box1() {
   return (
     <div>
@@ -10,28 +9,30 @@ function Box1() {
 <div className="container">
 <div className="box-container">
 <div className="box-container2">
-
 <div className="box-1">
 <div className="box-positioning">
-           <a href="" className="gray-icon"><img src={ Positionering1 } alt="Two Storeaid employees organizing storage boxes"/></a>
-           <a href="" className="green-icon"><img src={ Positionering2 } alt="Icon representing years of experience, satisfied clients, and number of warehouses"/>
+
+<div className="gray-icon">
+  <img src={Positionering1} alt="Two Storeaid employees organizing storage boxes" />
+</div>
+<div className="green-icon">
+  <img src={Positionering2} alt="Icon representing years of experience, satisfied clients, and number of warehouses" />
+</div>
            
            <div className="number-text-container"> 
-
             <div className="small-number-container"> 
             <h2 className="numbers">12+</h2>
             <div className="numbers-text">Years of Experience</div>
-</div>
+        </div>
     <div className="small-number-container"> 
         <h2 className="numbers">150k+</h2>
         <div className="numbers-text">Satisfied Clients</div>
-</div>
+    </div>
     <div className="small-number-container"> 
         <h2 className="numbers">35+</h2>
         <div className="numbers-text">Warehouse</div>
     </div>
 </div>   
-           </a>
         
         
         </div>
